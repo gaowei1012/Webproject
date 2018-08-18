3f49f5e7b082cf51cc9fa4b2f0297e1c17904944
gmw-zjw <gaomingwei2017@163.com>
2018-08-18 22:44:15 +0800

    city router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="header">
+        城市选择
+        <router-link to="/">
+            <span class="iconfont city-back-icon">&#xe624;</span>
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CityHeader'
+}
+</script>
+
+
+<style lang='stylus' scoped>
+@import './../../../assets/styles/varibles.styl'
+    .header
+        position: relative
+        overflow: hidden
+        height: .86rem
+        display: block
+        line-height: .86rem
+        text-align: center
+        background: @bgColor
+        font-size: .4rem
+        color: #fff
+        .city-back-icon
+            position: absolute
+            top: 0
+            left: 0
+            text-align: center
+            font-size: .4rem
+            margin-left: .1rem
+            color: #fff
+</style>
+
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
             <span class="iconfont">&#xe632;</span>
             输入你要的城市
         </div>
-        <div class="header-right">
-            上海
-            <span class="iconfont arrow-icon">&#xe64a;</span>
-        </div>
+        <router-link to='/city'>
+            <div class="header-right">
+                上海
+                <span class="iconfont arrow-icon">&#xe64a;</span>
+            </div>
+        </router-link>
     </div>
 </template>
 
@@ -48,6 +50,7 @@ export default {
         width: 1.24rem
         float: right 
         text-align: center
+        color: #fff
         .arrow-icon
             font-size: .24rem
             margin-left: -.04rem
