573b341b46dcbb03f123c646e76af5130fe0ebcd
gmw-zjw <gaomingwei2017@163.com>
2018-08-18 23:49:30 +0800

    city-list

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,18 +2,21 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list></city-list>
     </div>    
 </template>
 
 <script>
 import CityHeader from './../city/components/Header'
 import CitySearch from './../city/components/CitySearch'
+import CityList from './../city/components/List'
 
 export default {
     name: 'City',
     components: {
         CityHeader,
-        CitySearch
+        CitySearch,
+        CityList
     }
 }
 </script>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,151 @@
+<template>
+    <div class="list">
+        <div class="area">
+            <div class="title border-topbottom">当前城市</div>
+            <div class="button-list">
+                <div class="button-wapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">热门城市</div>
+            <div class="button-list">
+                <div class="button-wapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">A</div>
+            <div class="item-list">
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">A</div>
+            <div class="item-list">
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">A</div>
+            <div class="item-list">
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">A</div>
+            <div class="item-list">
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+                <div class="item">西安</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CityList'
+}
+</script>
+
+
+<style lang='stylus' scoped>
+@import './../../../assets/styles/varibles.styl'
+    .border-topbottom
+        &:before
+            border-color: #ccc
+        &:after
+            border-color: #ccc
+    .list
+        overflow: hidden
+        position: absolute
+        top: 1.58 rem
+        left: 0
+        right: 0
+        bottom: 0
+        .title
+            line-height: .44rem
+            background: #eee
+            padding-left: .26rem
+            color: #666 
+        .button-list
+            padding: .1rem .6rem .1rem .1rem
+            overflow: hidden
+            .button-wapper
+                float: left
+                margin-right: .19rem
+                width: 30.33%
+                .button
+                    padding: .1rem
+                    margin: .1rem 0
+                    text-align: center
+                    border: .02rem solid #ccc
+                    border-radius: .06rem
+        .item-list
+            .item
+                line-height: .76rem
+                color: #666
+                padding-left: .2rem
+                border-bottom: .01rem solid #eee    
+
+</style>
+
