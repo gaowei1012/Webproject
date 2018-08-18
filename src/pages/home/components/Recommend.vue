bf07d06811c59537a70629138ce388e84178cbf6
gmw-zjw <gaomingwei2017@163.com>
2018-08-18 18:23:26 +0800

    add recommend and wennker

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="wapper">
+        <div class='recommend-title'>热销推荐</div>
+        <ul>
+            <li class="item" v-for="item of recommendList" :key="item.id">
+                <img class="img-content" :src="item.imgUrl" alt="">
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+                    <button type="button" class="item-btn">查看详情</button>
+                </div>
+            </li>
+        </ul>
+    </div> 
+</template>
+
+<script>
+export default {
+  name: "HomeRecommend",
+  data() {
+    return {
+      recommendList: [
+        {
+          id: "001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1505/a1/a1950d296c2d10ca.water.jpg_200x200_86b5ff02.jpg",
+          title: "碧海金沙水上乐园",
+          desc: "描述"
+        },
+        {
+          id: "002",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1505/a1/a1950d296c2d10ca.water.jpg_200x200_86b5ff02.jpg",
+          title: "碧海金沙水上乐园",
+          desc: "描述"
+        },
+        {
+          id: "003",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1505/a1/a1950d296c2d10ca.water.jpg_200x200_86b5ff02.jpg",
+          title: "碧海金沙水上乐园",
+          desc: "描述"
+        },
+        {
+          id: "004",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1505/a1/a1950d296c2d10ca.water.jpg_200x200_86b5ff02.jpg",
+          title: "碧海金沙水上乐园",
+          desc: "描述"
+        },
+        {
+          id: "005",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1505/a1/a1950d296c2d10ca.water.jpg_200x200_86b5ff02.jpg",
+          title: "碧海金沙水上乐园",
+          desc: "描述"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import './../../../assets/styles/mixins.styl';
+
+.wapper {
+    .recommend-title {
+        margin-top: 0.2rem;
+        line-height: 0.8rem;
+        text-align: center;
+        background: #eee;
+    }
+
+    .item {
+        overflow: hidden;
+        display: flex;
+        height: 1.9rem;
+        border-bottom: .001rem solid #eee
+
+        .img-content {
+            width: 1.7rem;
+            height: 1.7rem;
+            padding: 0.1rem;
+        }
+
+        .item-info {
+            flex: 1;
+            padding: 0.1rem;
+            min-width: 0;
+
+            .item-title {
+                line-height: 0.54rem;
+                font-size: 0.32rem;
+                ellipsis();
+            }
+
+            .item-desc {
+                line-height: 0.4rem;
+                color: #ccc;
+                ellipsis();
+            }
+
+            .item-btn {
+                line-height: 0.44rem;
+                margin-top: 0.16rem;
+                background: #ff9300;
+                padding: 0 0.2rem;
+                border-radius: 0.06rem;
+                color: #fff;
+            }
+        }
+    }
+}
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="wapper">
+        <div class='recommend-title'>周末去哪</div>
+        <ul>
+            <li class="item" v-for="item of recommendList" :key="item.id">
+                <div class="item-img-wapper">
+                    <img class="img-content" :src="item.imgUrl" alt="">
+                </div>
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+                </div>
+            </li>
+        </ul>
+    </div> 
+</template>
+
+<script>
+export default {
+  name: "HomeWeekend",
+  data() {
+    return {
+      recommendList: [
+        {
+          id: "001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg",
+          title: "碧海金沙水上乐园",
+          desc: "描述"
+        },
+        {
+          id: "002",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg",
+          title: "碧海金沙水上乐园",
+          desc: "描述"
+        },
+        {
+          id: "003",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg",
+          title: "碧海金沙水上乐园",
+          desc: "描述"
+        },
+        {
+          id: "004",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg",
+          title: "碧海金沙水上乐园",
+          desc: "描述"
+        },
+        {
+          id: "005",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg",
+          title: "碧海金沙水上乐园",
+          desc: "描述"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import './../../../assets/styles/mixins.styl';
+ 
+    .recommend-title 
+        margin-top: 0.2rem;
+        line-height: 0.8rem;
+        text-align: center;
+        background: #eee;
+    .item-img-wapper
+        overflow: hidden
+        height: 0
+        padding-bottom: 33.9%
+        .img-content 
+            width: 100%
+
+    .item-info 
+        padding: 0.1rem
+        .item-title 
+            line-height: 0.54rem
+            font-size: 0.32rem
+            ellipsis()
+        .item-desc 
+            line-height: 0.4rem
+            color: #ccc
+            ellipsis()
+</style>
