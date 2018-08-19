111b0a63da6c97143ea2f209a61f63aaef8d3feb
gmw-zjw <gaomingwei2017@163.com>
2018-08-19 18:05:46 +0800

    city-kepplalive

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
     name: 'Home',
     components: {
@@ -29,13 +30,14 @@ export default {
             swiperList: [],
             iconList: [],
             recommendList: [],
-            weekendList: []
+            weekendList: [],
+            lastCity: ''
         }
 
     },
     methods: {
         getHomeInfo () {
-            axios.get('/api/index.json')
+            axios.get('/api/index.json?city=' + this.city)
                 .then(this.getHomeInfoSucc)
         },
         getHomeInfoSucc (res) {
@@ -49,8 +51,18 @@ export default {
             }
         }
     },
+    computed: {
+        ...mapState(['city'])
+    },
     mounted () {
+        this.lastCity = this.city,
         this.getHomeInfo()
+    },
+    activated () {
+        if (this.lastCity !== this.city) {
+            this.lastCity = this.city
+            this.getHomeInfo()
+        }
     }
 }
 </script>
