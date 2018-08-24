f51e5f51ec77b33abd6981049d2a3429ccd00ebb
gmw-zjw <gaomingwei2017@163.com>
2018-08-24 09:13:20 +0800

    detail-header

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -1,16 +1,57 @@
 <template>
     <div>
-        <router-link tag="div" to="/" class="header-abs">
+        <router-link 
+            tag="div"
+            to="/"
+            class="header-abs"
+            v-show="showAbs"    
+        >
             <div class="iconfont header-abs-back">&#xe624;</div>
         </router-link>
-        <div class="header-flexd"></div>
+        <div 
+            class="header-fixed"
+            v-show="!showAbs"
+            :style="opacityStyle"
+        >
+            <router-link to="/">
+                <div class="iconfont header-fixed-back">&#xe624;</div>
+            </router-link>
+        </div>
     </div>
 </template>
 
 
 <script>
 export default {
-  neme: "DetailHeader"
+  neme: "DetailHeader",
+  data () {
+      return {
+          showAbs: true,
+          opacityStyle: {
+              opactiy: 0
+          }
+      }
+  },
+  methods: {
+      handleScroll () {
+          // 滑动距离顶部的距离
+          const top = document.documentElement.scrollTop
+          console.log(top)
+          if (top > 60) {
+              let opacity = top / 140
+              opacity = opacity > 1 ? 1 : opacity // opacity
+              this.opacityStyle = {
+                  opacity
+              }
+              this.showAbs = false
+          } else {
+              this.showAbs = true
+          }
+      }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
 };
 </script>
 
@@ -28,6 +69,23 @@ export default {
         .header-abs-back
             color: #fff
             font-size: .4rem
-
+    .header-fixed
+        position: fixed
+        top: 0
+        left: 0
+        right: 0
+        height: .86rem
+        line-height: .86rem
+        text-align: center
+        color: #fff
+        background: #00bcd4
+        font-size: .32rem
+        .header-fixed-back
+            position: absolute
+            top: 0
+            left: 0
+            width: .64rem
+            margin-left: .1rem
+            color: #fff
 </style>
 
